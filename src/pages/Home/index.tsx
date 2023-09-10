1b7a6b6ff7fb7b59adc5ac48882f7233c5467b83
Volodymyr Fouchek <vinvova555333@gmail.com>
2023-09-10 17:49:56 +0300

    Create Board components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,12 +3,14 @@
 import Banner from '../../layouts/Banner';
 import NavBar from '../../layouts/NavBar';
 import rickAndMorty from '../../assets/rick-morty-grey.svg';
+import Board from './components/Board';
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <Banner title="The Rick and Morty API" imagePath={rickAndMorty} />
+      <Board />
     </div>
   );
 };
