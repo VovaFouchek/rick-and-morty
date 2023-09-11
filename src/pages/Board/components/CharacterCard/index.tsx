9b54f7ad7eefcc7680ce98076529cb8628b2643a
Volodymyr Fouchek <vinvova555333@gmail.com>
2023-09-11 15:41:20 +0300

    Update CharacterCard

--- src/pages/Board/components/CharacterCard/index.tsx
+++ src/pages/Board/components/CharacterCard/index.tsx
@@ -1,13 +1,21 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+import { useNavigate } from 'react-router-dom';
 import { Character } from '../../../../shared/types';
 import s from './characterCard.module.scss';
 
 interface CharacterCardProps {
   character: Character;
+  details?: boolean;
 }
 
 const CharacterCard: React.FC<CharacterCardProps> = ({
   character,
+  details,
 }): JSX.Element => {
+  const navigate = useNavigate();
+
   const checkIndicator = (): string => {
     if (character.status === 'Alive') {
       return s.green;
@@ -18,15 +26,27 @@ const CharacterCard: React.FC<CharacterCardProps> = ({
     return s.grey;
   };
 
+  const changeStylesForDetails = () => {
+    if (details) {
+      return s.detailCard;
+    }
+    return '';
+  };
+
+  const handleNavigate = (): void => navigate(`/${character.id}`);
+
   return (
-    <div className={s.card}>
+    <div className={`${s.card} ${changeStylesForDetails()}`}>
       <img
         src={character.image}
         alt={character.name}
-        className={s.card__image}
+        className={`${s.card__image} ${changeStylesForDetails()}`}
       />
+
       <div className={s.card__wrap}>
-        <strong className={s.card__title}>{character.name}</strong>
+        <strong onClick={handleNavigate} className={s.card__title}>
+          {character.name}
+        </strong>
         <div className={s.card__status}>
           <p className={`${s.card__indicator} ${checkIndicator()}`} />
           <p>
@@ -37,6 +57,12 @@ const CharacterCard: React.FC<CharacterCardProps> = ({
         <p className={s.card__text}>{character.location.name}</p>
         <span className={s.card__subtitle}>First seen in:</span>
         <p className={s.card__text}>{character.origin.name}</p>
+        {details && (
+          <>
+            <span className={s.card__subtitle}>Other Info</span>
+            <p className={s.card__text}>{character.gender}</p>
+          </>
+        )}
       </div>
     </div>
   );
