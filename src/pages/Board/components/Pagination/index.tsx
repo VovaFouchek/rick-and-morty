cc420cf054920e1d9aefb11d3ae209a4c958b786
Volodymyr Fouchek <vinvova555333@gmail.com>
2023-09-11 15:43:51 +0300

    Update Pagination, add to Redux data

--- src/pages/Board/components/Pagination/index.tsx
+++ src/pages/Board/components/Pagination/index.tsx
@@ -1,17 +1,17 @@
 import { Pagination, Stack, styled } from '@mui/material';
+
+import { useAppDispatch } from '../../../../redux/hook';
+import { setCurrentPage } from '../../../../redux/reducer';
 import styles from './pagination.module.scss';
 
 interface PaginationProps {
   pageAmount: number;
   page: number;
-  setPage: (value: number) => void;
 }
 
-const CustomPagination: React.FC<PaginationProps> = ({
-  pageAmount,
-  page,
-  setPage,
-}) => {
+const CustomPagination: React.FC<PaginationProps> = ({ pageAmount, page }) => {
+  const dispatch = useAppDispatch();
+
   const MyPagination = styled(Pagination)({
     '& .Mui-selected': {
       backgroundColor: '#F5F5F5 !important',
@@ -27,6 +27,10 @@ const CustomPagination: React.FC<PaginationProps> = ({
         '0px 1px 5px 0px rgba(0, 0, 0, 0.12), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 1px 0px rgba(0, 0, 0, 0.20)',
     },
   });
+
+  const handleChange = (_: unknown, number: number) => {
+    dispatch(setCurrentPage(number));
+  };
 
   return (
     <div className={styles.inner}>
@@ -44,9 +48,7 @@ const CustomPagination: React.FC<PaginationProps> = ({
             <MyPagination
               count={pageAmount}
               page={page}
-              onChange={(_, number) => {
-                setPage(number);
-              }}
+              onChange={handleChange}
               variant="outlined"
               shape="rounded"
               sx={{ justifyContent: 'center' }}
