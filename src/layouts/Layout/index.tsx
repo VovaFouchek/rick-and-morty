013f054488e9509cd8751c7077a6adad3c7c1f33
Volodymyr Fouchek <vinvova555333@gmail.com>
2023-09-14 14:52:59 +0300

    Add Lazy Loading & condit. rerder for Fab

--- src/layouts/Layout/index.tsx
+++ src/layouts/Layout/index.tsx
@@ -5,14 +5,17 @@ import NavBar from '../NavBar';
 import Banner from '../Banner';
 import rickAndMorty from '../../assets/rick-morty-grey.svg';
 import PopUpContent from '../../pages/Home/components/PopUpContent';
-import Fab from '../../components/Fab';
 
 import s from './layout.module.scss';
+import { useAppSelector } from '../../redux/hook';
 
 const PopUpComponent = lazy(() => import('../../components/PopUp'));
+const FabComponent = lazy(() => import('../../components/Fab'));
 
 const Layout = () => {
   const [openModal, setOpenModal] = useState(false);
+
+  const { isLoading } = useAppSelector((state) => state.characters);
 
   const handleOpenPopUp = () => setOpenModal(true);
   const handleClosePopUp = () => setOpenModal(false);
@@ -23,7 +26,7 @@ const Layout = () => {
       <Banner title="The Rick and Morty API" imagePath={rickAndMorty} />
       <div>
         {/* <CSVLink data={characters}>Download me</CSVLink>; */}
-        <Fab handleOpenPopUp={handleOpenPopUp} />
+        {!isLoading && <FabComponent handleOpenPopUp={handleOpenPopUp} />}
       </div>
       <PopUpComponent onClose={handleClosePopUp} openModal={openModal}>
         <PopUpContent onClose={handleClosePopUp} />
